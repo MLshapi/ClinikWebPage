1540dbd1cdc7e05e23dc3b601e764253eee54627
Marjanaupama <wyzhip-gygvyg-7sasMe>
2020-04-11 01:29:49 -0400

    refactoring

--- includes/add_patient.php
+++ includes/add_patient.php
@@ -2,7 +2,7 @@
 	include_once 'dbh.php';
 	$first = $_POST['first'];
 	$last = $_POST['last'];
-	$sql = "INSERT INTO patient(`first_name`,`last_name`) VALUES ('$first' , '$last');";
+	$sql = "INSERT INTO patient(`PatientFirstName`,`PatientLastName`) VALUES ('$first' , '$last');";
     mysqli_query($conn, $sql);
 
 
