ab9ad1afbdeb8162b812a05a894cb40cccbcad37
Marjanaupama <wyzhip-gygvyg-7sasMe>
2020-04-11 01:28:57 -0400

    refactoring

--- includes/modApp.php
+++ includes/modApp.php
@@ -2,20 +2,18 @@
 	include_once 'dbh.php';
 	$docFName = $_POST['docFName'];
 	$docLName = $_POST['docLName'];
-	$appDate = $_POST['appDate'];
+	$appDate = date("Y-m-d H:i:s",strtotime($_POST['appDate']));
 	$pFName = $_POST['first'];
 	$pLName = $_POST['last'];
-	$nDate = $_POST['nDate'];
+	$nDate = date("Y-m-d H:i:s",strtotime($_POST['nDate']));
 
-    $sql1 = "SELECT p_id FROM patient WHERE first_name = '$pFName' AND last_name = '$pLName';";
+    $sql1 = "SELECT PatientID FROM patient WHERE PatientFirstName = '$pFName' AND PatientLastName = '$pLName';";
     $result = mysqli_query($conn, $sql1);
     $arr_patient = mysqli_fetch_row($result);
-
-	$sql2 = "SELECT d_id, c_id FROM dentist WHERE first_name = '$docFName' AND last_name = '$docLName';";
+	$sql2 = "SELECT EmployeeID, ClinicID FROM employee WHERE EmployeeFirstName = '$docFName' AND EmployeeLastName = '$docLName';";
 	$result2 = mysqli_query($conn, $sql2);
 	$arr_dentist = mysqli_fetch_row($result2);
-
-	$sql = "UPDATE appointment SET date = '$nDate' WHERE p_id = '$arr_patient[0]' AND d_id = '$arr_dentist[0]' AND date = '$appDate';";
+	$sql = "UPDATE appointment SET AppointmentDateTime = '$nDate' WHERE PatientID = '$arr_patient[0]' AND DentistEmployeeID = '$arr_dentist[0]' AND AppointmentDateTime  = '$appDate';";
     mysqli_query($conn, $sql);
 	
-	header("Location: ../dmp.php");
+	//header("Location: ../dmp.php");
