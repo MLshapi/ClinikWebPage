bfadc21c8e2834a0f614aae27879840265e36144
Marjanaupama <wyzhip-gygvyg-7sasMe>
2020-04-11 01:29:08 -0400

    refactoring

--- includes/delApp.php
+++ includes/delApp.php
@@ -2,19 +2,19 @@
 	include_once 'dbh.php';
 	$docFName = $_POST['docFName'];
 	$docLName = $_POST['docLName'];
-	$appDate = $_POST['appDate'];
+	$appDate = date("Y-m-d H:i:s",strtotime($_POST['appDate']));
 	$pFName = $_POST['first'];
 	$pLName = $_POST['last'];
 
-    $sql1 = "SELECT p_id FROM patient WHERE first_name = '$pFName' AND last_name = '$pLName';";
+    $sql1 = "SELECT PatientID FROM patient WHERE PatientFirstName = '$pFName' AND PatientLastName = '$pLName';";
     $result = mysqli_query($conn, $sql1);
     $arr_patient = mysqli_fetch_row($result);
 
-	$sql2 = "SELECT d_id, c_id FROM dentist WHERE first_name = '$docFName' AND last_name = '$docLName';";
+	$sql2 = "SELECT EmployeeID, ClinicID FROM employee WHERE EmployeeFirstName = '$docFName' AND EmployeeLastName = '$docLName';";
 	$result2 = mysqli_query($conn, $sql2);
 	$arr_dentist = mysqli_fetch_row($result2);
 
-	$sql = "DELETE FROM appointment WHERE p_id = '$arr_patient[0]' AND d_id = '$arr_dentist[0]' AND date = '$appDate';";
+	$sql = "DELETE FROM appointment WHERE PatientID = '$arr_patient[0]' AND DentistEmployeeID = '$arr_dentist[0]' AND AppointmentDateTime = '$appDate';";
     mysqli_query($conn, $sql);
     
 	header("Location: ../dmp.php");
